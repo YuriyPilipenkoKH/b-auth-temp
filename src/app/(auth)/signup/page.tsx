bdc714b95f04eecabeb44af89de438d5b46cc948
Yuriy Pilipenko <yurik2061@gmail.com>
2025-11-30 10:30:20 +0200

    showed signupform in ui

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,8 +1,16 @@
+import SignUpForm from '@/components/SignUpForm'
 import React from 'react'
 
 function SignupPage() {
   return (
-    <div>SignupPage</div>
+         <div className='flex flex-col gap-5 items-center justify-center   p-6'  >
+        <div className='flex flex-col gap-5 w-[400px]'>
+          <h1 className='text-3xl font-bold text-center'>Create a New Account</h1>
+          
+        </div>
+
+      <SignUpForm/>
+    </div>
   )
 }
 
