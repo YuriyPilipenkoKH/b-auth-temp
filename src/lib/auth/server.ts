f95e35badace5106e81b02c47c1167e4f38fa39c
Yuriy Pilipenko <yurik2061@gmail.com>
2025-11-30 09:58:15 +0200

    file structure

--- src/lib/auth/server.ts
+++ src/lib/auth/server.ts
@@ -1,6 +1,6 @@
 
 import { headers } from "next/headers";
-import { auth } from "../../auth";
+import { auth } from "../../../auth";
 
 export const session = await auth.api.getSession({
     headers: await headers() // you need to pass the headers object.
