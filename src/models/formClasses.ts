5ce6fbb5bf979e3d8fb498ece2cdea4caaa0a24c
Yuriy Pilipenko <yurik2061@gmail.com>
2025-12-02 10:41:45 +0200

    FormClassNames created

---
+++ src/models/formClasses.ts
@@ -0,0 +1,16 @@
+export const formClasses = {
+  container: 'form-container',
+  error: 'form-error',
+  loading: 'form-loading',
+  spinner: 'spinner',
+  space: 'space',
+  form: 'form',
+  inputContainer: 'input-container',
+  input: 'form-input',
+  submitButton: 'submit-button',
+
+} as const;
+
+// Create a type from the values
+export type FormClassName = typeof formClasses[keyof typeof formClasses];
+
