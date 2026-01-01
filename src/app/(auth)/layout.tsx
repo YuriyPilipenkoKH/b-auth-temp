b5f597f863bcc66afb2bb69a6bd272ca8f74eb04
Yuriy Pilipenko <yurik2061@gmail.com>
2026-01-01 18:43:41 +0200

    applied layout for login

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,19 @@
 
- function AuthLayout() {
+ function AuthLayout(
+  { children }: { children: React.ReactNode } 
+ ) {
 
   return (
-    <div className=' h-full flex flex-col gap-4 items-center  text-slate-300 bg-[var(--auth-bg)]'>
-      <div className='absolute top-[1rem] left-[1rem]'>
-      </div>
-
+    <div
+      className="min-h-screen flex items-center justify-center bg-cover bg-center"
+      style={{
+        backgroundImage:
+          "url('https://res.cloudinary.com/dwdkw1a4j/image/upload/v1767283634/b-auth-temp/img/nrurpajrd1ivzf9i7unj.jpg')",
+      }}
+    >
+      {/* <div className='absolute top-[1rem] left-[1rem]'> */}
+      {/* </div> */}
+        {children}
     </div>
   )
 }
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -4,15 +4,7 @@ import Link from 'next/link'
 
 function SignupPage() {
   return (
-    <div
-      className="min-h-screen flex items-center justify-center bg-cover bg-center"
-      style={{
-        backgroundImage:
-          "url('https://res.cloudinary.com/dwdkw1a4j/image/upload/v1767283634/b-auth-temp/img/nrurpajrd1ivzf9i7unj.jpg')",
-      }}
-    >
-      {/* optional dark overlay */}
-      {/* <div className="absolute inset-0 bg-black/50" /> */}
+
 
       <div className="relative z-10 flex flex-col gap-5 items-center justify-center p-6">
         <div className="flex flex-col gap-5 w-[400px]">
@@ -29,7 +21,7 @@ function SignupPage() {
             {" "}Login
           </Link>
         </p>
-      </div>
+
     </div>
   )
 }
