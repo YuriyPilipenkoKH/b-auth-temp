362db45d0598751d86130186602fd781bed3da50
Yuriy Pilipenko <yurik2061@gmail.com>
2026-01-01 17:41:24 +0200

    signup link

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 
 
 function LoginPage() {
@@ -7,6 +8,9 @@ function LoginPage() {
           <h1 className='text-3xl font-bold text-center'>Login to Your Account</h1>
           {/* <SignInButton provider='google' />
           <SignInButton provider='github' /> */}
+           <p className='text-center'>Don`t have an account? 
+            <Link href="/signup" className="hover:text-amber-400">{" "}SignUp</Link>
+          </p>
         </div>
 
       {/* <SignInForm/> */}
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -6,6 +6,8 @@ function SignupPage() {
          <div className='flex flex-col gap-5 items-center justify-center   p-6'  >
         <div className='flex flex-col gap-5 w-[400px]'>
           <h1 className='text-3xl font-bold text-center'>Create a New Account</h1>
+
+         
           
         </div>
 
