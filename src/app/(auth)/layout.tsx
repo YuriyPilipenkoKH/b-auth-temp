dc7cde2e51f35783d31dbfb317bd86bc09a03e7f
Yuriy Pilipenko <yurik2061@gmail.com>
2026-01-01 18:35:06 +0200

    AuthLayout

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+
+ function AuthLayout() {
+
+  return (
+    <div className=' h-full flex flex-col gap-4 items-center  text-slate-300 bg-[var(--auth-bg)]'>
+      <div className='absolute top-[1rem] left-[1rem]'>
+      </div>
+
+    </div>
+  )
+}
+
+export default AuthLayout
