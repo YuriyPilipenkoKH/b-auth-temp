7e43c7001f73e37ae637c80042826ce226485152
Yuriy Pilipenko <yurik2061@gmail.com>
2026-01-02 17:46:03 +0200

    signUpUser server action add

---
+++ src/actions/signup.ts
@@ -0,0 +1,10 @@
+"use server"
+
+
+export async function signUpUser(formData: FormData) {
+  const name = formData.get("name") as string;
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;  
+
+  
+}
