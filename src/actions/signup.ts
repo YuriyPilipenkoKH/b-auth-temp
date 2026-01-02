e0f9dfa74f7086ffdb7f6b673bbb22c11307866f
Yuriy Pilipenko <yurik2061@gmail.com>
2026-01-02 17:51:01 +0200

    signUpEmail completed

--- src/actions/signup.ts
+++ src/actions/signup.ts
@@ -1,10 +1,19 @@
 "use server"
 
+import { auth } from "../../auth";
+
 
 export async function signUpUser(formData: FormData) {
   const name = formData.get("name") as string;
   const email = formData.get("email") as string;
   const password = formData.get("password") as string;  
 
-  
+  await auth.api.signUpEmail({
+    body: {
+      name,
+      email,
+      password,
+      callbackURL: "/dashboard",
+    },
+  });
 }
