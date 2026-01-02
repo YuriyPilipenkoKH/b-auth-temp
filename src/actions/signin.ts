2f90e4290bf2eb2f1c691bee743aaacf746a1b96
Yuriy Pilipenko <yurik2061@gmail.com>
2026-01-02 17:54:30 +0200

    signInUser action done

--- src/actions/signin.ts
+++ src/actions/signin.ts
@@ -4,14 +4,13 @@ import { redirect } from "next/navigation";
 import { auth } from "../../auth";
 
 
-export async function signUpUser(formData: FormData) {
-  const name = formData.get("name") as string;
+export async function signInUser(formData: FormData) {
+
   const email = formData.get("email") as string;
   const password = formData.get("password") as string;  
 
-  await auth.api.signUpEmail({
+  await auth.api.signInEmail({
     body: {
-      name,
       email,
       password,
 
