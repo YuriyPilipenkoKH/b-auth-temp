e211f601d689c63a58bb92c38b559727a30b77bb
Yuriy Pilipenko <yurik2061@gmail.com>
2026-01-05 16:42:58 +0200

    rm protection to layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,12 @@
+import { getServerSession } from "@/lib/getServerSession";
+import { redirect } from "next/navigation";
 
- function AuthLayout(
+
+ async function AuthLayout(
   { children }: { children: React.ReactNode } 
  ) {
+     const session = await getServerSession();
+      if (session)   redirect('/dashboard')
 
   return (
     <div
