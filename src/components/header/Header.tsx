a701e280c3ee5c396e0690c6bb3c913536fbe03c
Yuriy Pilipenko <yurik2061@gmail.com>
2026-01-07 11:32:58 +0200

    logo enabed

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import Logo from "./Logo"
 
 
 
@@ -6,7 +7,7 @@ const Header = () => {
   return (
     <header className='py-8 xl:py-12 text-white bg-primary '>
       <div className="container mx-auto flex justify-between items-center">
-        {/* <Logo/> */}
+        <Logo />
         <div className='hidden xl:flex items-center gap-8 '>
           {/* <Nav/> */}
           <Link href={'/contact'}  >
