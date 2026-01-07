6a2a63a1a437e66354f5e9e2f62673dd8718d845
Yuriy Pilipenko <yurik2061@gmail.com>
2026-01-07 11:31:19 +0200

    logo add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import { options } from "@/lib/hotToast";
+import Header from "@/components/header/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header/>
         {children}
          <Toaster 
           position="top-center" 
---
+++ src/components/header/Logo.tsx
@@ -0,0 +1,35 @@
+"use client"
+import Link from 'next/link'
+import { useState } from 'react'
+
+
+
+const Logo = () => {
+const [anable, setAnable] = useState<boolean>(false)
+const rclick = (event: React.MouseEvent) => {
+  event.preventDefault(); // Prevents the default right-click menu
+  setAnable(!anable);
+  };
+
+return (
+  <>
+    <Link href={"/"} onContextMenu={rclick}>
+      <div className="flex items-baseline text-4xl font-semibold">
+
+        {!anable ? (
+            <h1 className="">Portfolio</h1>
+        ) : (
+          <>
+            <h1 className="h12">Portfolio</h1>
+            <h1 className="h10">Pf</h1> {/* Always show 'Pf' when anable is true */}
+          </>
+        )}
+        <span className="text-accent">.</span>
+      </div>
+    </Link>
+    {/* {anable && <AuthForm setAnable={setAnable}/>} */}
+  </>
+);
+}
+
+export default Logo
