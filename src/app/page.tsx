60800924466107f0fff6bc686297cdc8dba86ab6
Yuriy Pilipenko <yurik2061@gmail.com>
2026-01-07 13:14:03 +0200

    home page update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
+    <main className="flex min-h-screen flex-col items-center  gap-6 pt-12">
       <h1 className="text-4xl font-bold">Home Page</h1>
-      <Link href="/login">Login</Link>
+      <Link href="/login">Get started</Link>
       <p className="mt-4 text-lg">This is a sample Next.js application with authentication provided by Better Auth.</p>
     </main>
   );
