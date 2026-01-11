2d5c2e1f4fee87fa527c6cde0c0b408e9f2166ac
Yuriy Pilipenko <yurik2061@gmail.com>
2026-01-11 11:57:07 +0200

    trying to retrieve searchparams

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,13 +1,22 @@
 import Header from "@/components/header/Header";
 import { getServerSession } from "@/lib/getServerSession";
+
 import { redirect } from "next/navigation";
+
+interface AuthLayoutProps {
+  children: React.ReactNode;
+   SearchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}
 
 
  async function AuthLayout(
-  { children }: { children: React.ReactNode } 
+  { children, SearchParams }: AuthLayoutProps
  ) {
      const session = await getServerSession();
       if (session)   redirect('/dashboard')
+
+     const searchParams = await SearchParams
+    console.log('params :', searchParams);    
 
   return (
     <div
