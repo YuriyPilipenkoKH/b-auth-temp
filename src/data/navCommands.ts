96323adac68dce7c233656579eef32112eb17632
Yuriy Pilipenko <yurik2061@gmail.com>
2026-02-01 13:14:51 +0200

    retreived commandsArray from env

--- src/data/navCommands.ts
+++ src/data/navCommands.ts
@@ -4,6 +4,9 @@ export type NavCommand = {
   requiresAdmin?: boolean;
 };
 
+const commandsArray = process.env.NEXT_PUBLIC_NAV_COMMANDS?.split(',') || []
+
+
 export const NAV_COMMANDS: Record<string, NavCommand> = {
   home: { path: "/" },
   log: { path: "/login" },
